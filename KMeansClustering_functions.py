e17e3ea5804004179794a26250168d2945fdfa17
Jennifer Cross <jenncross@users.noreply.github.com>
2020-04-01 16:41:27 -0400

    update code

--- KMeansClustering_functions.py
+++ KMeansClustering_functions.py
@@ -1,1 +1,10 @@
 #Please place your FUNCTION code for step 4 here.
+
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+
+def openckdfile():
+    glucose, hemoglobin, classification = np.loadtxt('ckd.csv', delimiter=',', skiprows=1, unpack=True)
+    return glucose, hemoglobin, classification
--- NearestNeighborClassification.py
+++ NearestNeighborClassification.py
@@ -1,1 +1,27 @@
 #Please put your code for Step 2 and Step 3 in this file.
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+
+# FUNCTIONS
+def openckdfile():
+    glucose, hemoglobin, classification = np.loadtxt('ckd.csv', delimiter=',', skiprows=1, unpack=True)
+    return glucose, hemoglobin, classification
+
+
+
+
+
+# MAIN SCRIPT
+glucose, hemoglobin, classification = openckdfile()
+
+plt.figure()
+plt.plot(hemoglobin[classification==1],glucose[classification==1], "k.", label = "Class 1")
+plt.plot(hemoglobin[classification==0],glucose[classification==0], "r.", label = "Class 0")
+plt.xlabel("Hemoglobin")
+plt.ylabel("Glucose")
+plt.legend()
+plt.show()
